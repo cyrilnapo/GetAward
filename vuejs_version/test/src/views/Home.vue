c9c844e71d915ab20b04d2ac39c433e88aed3a60
Cyril Napoleone <napoleone.cyril@gmail.com>
2024-06-07 15:54:12 +0200

    vue project not working
    je dois m'inspirer de l'appli notes pour la config du projet

---
+++ vuejs_version/test/src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>Welcome to GetAward</h1>
+    <router-link to="/login">Login</router-link> | <router-link to="/signup">Sign Up</router-link> |
+    <router-link to="/tasks">View Tasks</router-link>
+  </div>
+</template>
